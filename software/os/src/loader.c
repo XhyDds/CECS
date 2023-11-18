a174c021a8350b158da0b7ab6f55ba556287acb3
XhyDds <1136759302@qq.com>
2023-11-18 17:15:34 +0800

    lab7 xuanzuo

--- software/os/src/loader.c
+++ software/os/src/loader.c
@@ -9,11 +9,38 @@
 
 static uintptr_t elf_load(const char *filename) {
     // Lab7 TODO: implement the loader
-    //1
+    // //1
+    // Elf_Ehdr elf_h;
+    // size_t  disk_offset=find_offset(filename);
+    // Log("disk_offset=%d",disk_offset);
+    // ramdisk_read((void*)&elf_h, disk_offset, sizeof(Elf_Ehdr));
+    // // assert(elf_h.e_ident[0] == 0x80);
+    // assert(elf_h.e_machine == EM_RISCV);
+    // //2
+    // Elf_Phdr elf_ph;
+    // int i;
+    // for(i=0;i<elf_h.e_phnum;i++){
+    //     // printf("%x:i:%d\n",&i,i);
+    //     if(i<0) break;
+    //     size_t num=ramdisk_read((void*)&elf_ph, disk_offset+elf_h.e_phoff+i*elf_h.e_phentsize, sizeof(Elf_Phdr));
+    //     // printf("i:%d\n",i);
+    //     if(elf_ph.p_type == PT_LOAD){
+    //         Log("elf_ph.p_vaddr:%x p_offset:%x",elf_ph.p_vaddr,elf_ph.p_offset);
+    //         ramdisk_read((void*)elf_ph.p_vaddr, disk_offset+elf_ph.p_offset, elf_ph.p_memsz);
+    //         Log("elf_ph.p_memsz:%x",elf_ph.p_memsz);
+    //         Log("elf_ph.p_filesz:%x",elf_ph.p_filesz);
+    //         memset((void*)(elf_ph.p_vaddr+elf_ph.p_filesz), 0, elf_ph.p_memsz-elf_ph.p_filesz);
+    //     }
+    //     // ph_offset+=elf_h.e_phentsize;
+    // }
+    // //3
+    // asm volatile("fence.i");
+    // return elf_h.e_entry;
+
+        //1
     Elf_Ehdr elf_h;
-    size_t  disk_offset=find_offset(filename);
-    Log("disk_offset=%d",disk_offset);
-    ramdisk_read((void*)&elf_h, disk_offset, sizeof(Elf_Ehdr));
+    int fd=fs_open(filename,0,0);
+    fs_read(fd, (void*)&elf_h, sizeof(Elf_Ehdr));
     // assert(elf_h.e_ident[0] == 0x80);
     assert(elf_h.e_machine == EM_RISCV);
     //2
@@ -22,11 +49,13 @@ static uintptr_t elf_load(const char *filename) {
     for(i=0;i<elf_h.e_phnum;i++){
         // printf("%x:i:%d\n",&i,i);
         if(i<0) break;
-        size_t num=ramdisk_read((void*)&elf_ph, disk_offset+elf_h.e_phoff+i*elf_h.e_phentsize, sizeof(Elf_Phdr));
+        fs_lseek(fd,elf_h.e_phoff+i*elf_h.e_phentsize,SEEK_SET);
+        fs_read(fd, (void*)&elf_ph, sizeof(Elf_Phdr));
         // printf("i:%d\n",i);
         if(elf_ph.p_type == PT_LOAD){
             Log("elf_ph.p_vaddr:%x p_offset:%x",elf_ph.p_vaddr,elf_ph.p_offset);
-            ramdisk_read((void*)elf_ph.p_vaddr, disk_offset+elf_ph.p_offset, elf_ph.p_memsz);
+            fs_lseek(fd,elf_ph.p_offset,SEEK_SET);
+            fs_read(fd, (void*)elf_ph.p_vaddr, elf_ph.p_memsz);
             Log("elf_ph.p_memsz:%x",elf_ph.p_memsz);
             Log("elf_ph.p_filesz:%x",elf_ph.p_filesz);
             memset((void*)(elf_ph.p_vaddr+elf_ph.p_filesz), 0, elf_ph.p_memsz-elf_ph.p_filesz);
