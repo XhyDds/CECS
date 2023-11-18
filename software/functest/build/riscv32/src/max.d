bca96d7cacf64bdc8f0c5724bb7a1bff4eb48317
XhyDds <1136759302@qq.com>
2023-11-18 17:05:59 +0800

    lab3 xuanzuo

--- software/functest/build/riscv32/src/max.d
+++
@@ -1,6 +0,0 @@
-/home/xhyvm2/gitwork/labs/COMPSYS/software/functest/build/riscv32/src/max.o: \
- /home/xhyvm2/gitwork/labs/COMPSYS/software/functest/src/max.c \
- /home/xhyvm2/gitwork/labs/COMPSYS/software/functest/include/trap.h \
- ../base-port/base/include/base.h ../base-port/base/include/arch.h \
- ../base-port/base/include/dev.h ../base-port/base/include/base-macro.h \
- ../base-port/tool/include/tool.h
